9fb4bff1ad87a5386a9eb53afd1c0dae80102921
d1y <chenhonzhou@gmail.com>
2021-09-23 05:33:36 +0800

    project init

---
+++ README.md
@@ -0,0 +1,68 @@
+# font_logos
+
+![](https://cdn.jsdelivr.net/gh/lukas-w/font-logos@master/assets/preview.png)
+
+Font-logos is an icon font containing logos of popular linux distributions and other open source software for inclusion in websites.
+
+Note: All brand icons are trademarks of their respective owners and should only be used to represent the company or product to which they refer.
+
+![Screen Shot 2021-09-23 at 05.29.01.png](https://i.loli.net/2021/09/23/GFRJ9PHeLpIlkou.png)
+
+add you `pubspec.yaml`
+
+```yaml
+dependencies:
+
+  macwindowctl:
+    git:
+      url: git://github.com/d1y/font_logos
+      ref: master
+```
+
+Available logos are:
+
+| Distribution  | CSS class                  | Inclusion code                          |
+| ------------- | -------------------------- | --------------------------------------- |
+| Alma Linux    | `fl_almalinux`             | `<span class="fl_almalinux"></span>`    |
+| Alpine        | `fl_alpine`                | `<span class="fl_alpine"></span>`       |
+| AOSC OS       | `fl_aosc`                  | `<span class="fl_aosc"></span>`         |
+| Arch Linux    | `fl_archlinux`             | `<span class="fl_archlinux"></span>`    |
+| ArchLabs      | `fl_archlabs`              | `<span class="fl_archlabs"></span>`     |
+| Artix Linux   | `fl_artix`                 | `<span class="fl_artix"></span>`        |
+| Apple         | `fl_apple`                 | `<span class="fl_apple"></span>`        |
+| Budgie        | `fl_budgie`                | `<span class="fl_budgie"></span>`       |
+| CentOS        | `fl_centos`                | `<span class="fl_centos"></span>`       |
+| CoreOS        | `fl_coreos`                | `<span class="fl_coreos"></span>`       |
+| Debian        | `fl_debian`                | `<span class="fl_debian"></span>`       |
+| Deepin        | `fl_deepin`                | `<span class="fl_deepin"></span>`       |
+| Devuan        | `fl_devuan`                | `<span class="fl_devuan"></span>`       |
+| Docker        | `fl_docker`                | `<span class="fl_docker"></span>`       |
+| elementary OS | `fl_elementary`            | `<span class="fl_elementary"></span>`   |
+| Fedora        | `fl_fedora[-inverse]`      | `<span class="fl_fedora"></span>`       |
+| Ferris        | `fl_ferris`                | `<span class="fl_ferris"></span>`       |
+| FreeBSD       | `fl_freebsd`               | `<span class="fl_freebsd"></span>`      |
+| Gentoo        | `fl_gentoo`                | `<span class="fl_gentoo"></span>`       |
+| illumos       | `fl_illumos`               | `<span class="fl_illumos"></span>`      |
+| Kali Linux    | `fl_kali-linux`            | `<span class="fl_kali-linux"></span>`   |
+| Linux Mint    | `fl_linuxmint[-inverse]`   | `<span class="fl_linuxmint"></span>`    |
+| Mageia        | `fl_mageia`                | `<span class="fl_mageia"></span>`       |
+| Manjaro       | `fl_manjaro`               | `<span class="fl_manjaro"></span>`      |
+| Mandriva      | `fl_mandriva`              | `<span class="fl_mandriva"></span>`     |
+| NixOS         | `fl_nixos`                 | `<span class="fl_nixos"></span>`        |
+| OpenBSD       | `fl_openbsd`               | `<span class="fl_openbsd"></span>`      |
+| OpenSUSE      | `fl_opensuse`              | `<span class="fl_opensuse"></span>`     |
+| Pop!_OS       | `fl_pop-os`                | `<span class="fl_pop-os"></span>`       |
+| Raspberry Pi  | `fl_raspberry-pi`          | `<span class="fl_raspberry-pi"></span>` |
+| Red Hat       | `fl_redhat`                | `<span class="fl_redhat"></span>`       |
+| Rocky Linux   | `fl_rocky-linux`           | `<span class="fl_rocky-linux"></span>`  |
+| Sabayon       | `fl_sabayon`               | `<span class="fl_sabayon"></span>`      |
+| Slackware     | `fl_slackware[-inverse]`   | `<span class="fl_slackware"></span>`    |
+| Solus         | `fl_solus`                 | `<span class="fl_solus"></span>`        |
+| Tux           | `fl_tux`                   | `<span class="fl_tux"></span>`          |
+| Ubuntu        | `fl_ubuntu[-inverse]`      | `<span class="fl_ubuntu"></span>`       |
+| Void          | `fl_void`                  | `<span class="fl_void"></span>`         |
+| Zorin OS      | `fl_zorin`                 | `<span class="fl_zorin"></span>`        |
+
+## copyright
+
+https://github.com/lukas-w/font-logos
---
+++ lib/font_logos.dart
@@ -0,0 +1,57 @@
+// auto generated DOT EDIT!!!
+  
+import 'package:flutter/widgets.dart';
+
+class FontLogos {
+  static const String _family = 'font-logos';
+  
+static const String _kFontPkg = 'font_logos';
+  FontLogos._();
+
+  static const IconData fl_alpine = IconData(0xf100, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_aosc = IconData(0xf101, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_apple = IconData(0xf102, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_archlabs = IconData(0xf127, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_archlinux = IconData(0xf103, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_artix = IconData(0xf12c, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_budgie = IconData(0xf122, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_centos = IconData(0xf104, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_coreos = IconData(0xf105, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_debian = IconData(0xf106, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_deepin = IconData(0xf123, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_devuan = IconData(0xf107, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_docker = IconData(0xf108, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_elementary = IconData(0xf109, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_fedora = IconData(0xf10a, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_fedora_inverse = IconData(0xf10b, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_ferris = IconData(0xf128, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_flathub = IconData(0xf11d, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_freebsd = IconData(0xf10c, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_gentoo = IconData(0xf10d, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_gnu_guix = IconData(0xf11e, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_illumos = IconData(0xf124, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_linuxmint = IconData(0xf10e, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_linuxmint_inverse = IconData(0xf10f, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_mageia = IconData(0xf110, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_mandriva = IconData(0xf111, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_manjaro = IconData(0xf112, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_nixos = IconData(0xf113, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_openbsd = IconData(0xf125, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_opensuse = IconData(0xf114, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_pop_os = IconData(0xf12b, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_raspberry_pi = IconData(0xf115, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_redhat = IconData(0xf116, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_sabayon = IconData(0xf117, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_slackware = IconData(0xf118, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_slackware_inverse = IconData(0xf119, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_snappy = IconData(0xf11f, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_solus = IconData(0xf126, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_tux = IconData(0xf11a, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_ubuntu = IconData(0xf11b, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_ubuntu_inverse = IconData(0xf11c, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_void = IconData(0xf120, fontFamily: _family, fontPackage: _kFontPkg);
+  static const IconData fl_zorin = IconData(0xf121, fontFamily: _family, fontPackage: _kFontPkg);
+
+}
+  
+// font download url: https:
